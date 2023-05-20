98ec9a075aba9c70bd059ab04847e81d74b734e0
csaba85 <mikcsabee@gmail.com>
2023-05-20 16:28:23 +0200

    fix private key generator

--- postman/build.ts
+++ postman/build.ts
@@ -75,8 +75,6 @@ async function generateFakeServiceAccount() {
     privateKeyEncoding: {
       type: "pkcs8",
       format: "pem",
-      cipher: "aes-256-cbc",
-      passphrase: "top secret",
     },
   }).privateKey;
 
