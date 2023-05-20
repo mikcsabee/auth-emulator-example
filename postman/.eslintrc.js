fc6717735a7b53e919ac6e0355c86e69fb6778d1
csaba85 <mikcsabee@gmail.com>
2023-05-20 11:26:06 +0200

    Compile the postman_pre-request_script.ts file and generate/test the PostmanCollection

---
+++ postman/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.postman.json", "tsconfig.test.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/dist/**/*", // Ignore built files.
+  ],
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
+    quotes: ["error", "double"],
+    "import/no-unresolved": 0,
+    indent: ["error", 2],
+    "@typescript-eslint/no-var-requires": "off",
+  },
+};
---
+++ postman/src/postman_pre-request_script.ts
@@ -0,0 +1,57 @@
+import type {Postman} from "postman-sandbox";
+
+declare const pm: Postman;
+
+const apiKey = pm.collectionVariables.get("apiKey");
+
+/**
+ * generates a random raw id
+ * @return { string }
+ */
+function randomProviderRawId() {
+  let str = "";
+  for (let i = 0; i < 40; i++) {
+    str += Math.floor(Math.random() * 10).toString();
+  }
+  return str;
+}
+
+const claims = encodeURIComponent(
+  JSON.stringify({
+    sub: randomProviderRawId(),
+    iss: "",
+    aud: "",
+    exp: 0,
+    iat: 0,
+    name: pm.collectionVariables.get("authName"),
+    email: pm.collectionVariables.get("authEmail"),
+    email_verified: true,
+    picture: "",
+  })
+);
+
+const payload = JSON.stringify({
+  requestUri: `http://localhost:9099/emulator/auth/handler?providerId=google.com&id_token=${claims}`,
+  returnIdpCredential: true,
+  returnSecureToken: true,
+  sessionId: "ValueNotUsedByAuthEmulator",
+});
+
+const options = {
+  url: `http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key=${apiKey}`,
+  method: "POST",
+  header: {"content-type": "application/json"},
+  body: {
+    mode: "raw",
+    raw: payload,
+  },
+};
+
+pm.sendRequest(options, function(err, res) {
+  if (err) {
+    console.error(err);
+    throw new Error("Unable to authenticate!");
+  } else {
+    pm.collectionVariables.set("token", res.json().idToken);
+  }
+});
